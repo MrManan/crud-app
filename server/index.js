ca593520dbee9aeab6d6962af02b2723dfc1cf9e
MrManan <mrmanan5657@gmail.com>
2023-03-31 23:56:26 +0500

    fix error

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/product", (req, res) => {
   const { name, price, categoryid } = req.body;
   const sql = "INSERT INTO product (name, price,categoryid) VALUES (?, ?,?)";
-  connection.query(sql, [name, price, categoryid], (err, result) => {
+  connection.query(sql, [name, price, +categoryid], (err, result) => {
     if (err) {
       return res.status(500).end(err);
     }
